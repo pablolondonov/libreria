4942849e4cb7912dcb63f9f3c040286a7f443240
julian uribe <juliancanyal2@gmail.com>
2024-06-01 20:40:38 -0500

    commit de julian
    estilizacion con boostrap de la vista de create new book

--- resources/views/Books/create.blade.php
+++ resources/views/Books/create.blade.php
@@ -3,24 +3,45 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Create Book</title>
     <!-- Agrega aquí tus enlaces CSS si los necesitas -->
 </head>
 <body>
     <h1>Create a New Book</h1>
 
-    {{-- @if ($errors->any())
-        <div>
-            <strong>Whoops! Something went wrong!</strong>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif --}}
+    <form  action="{{ route('books.store') }}" method="POST">
+    @csrf
+  <div class="mb-3" style="width: 400px;">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="text" class="form-control" id="name" name="name" required>
+  </div>
+  <div class="mb-3" style="width: 400px;">
+    <label for="exampleInputEmail1" class="form-label">Author</label>
+    <input type="text" class="form-control" id="author" name="author" required>
+  </div>
+  <div class="mb-3" style="width: 400px;">
+    <label for="exampleInputEmail1" class="form-label">Publisher</label>
+    <input type="text" class="form-control" id="publisher" name="publisher" required>
+  </div>
+  <div class="mb-3" style="width: 400px;">
+    <label for="exampleInputEmail1" class="form-label">Published Date</label>
+    <input type="date" class="form-control" id="published_date" name="published_date">
+  </div>
+  <div class="mb-3" style="width: 400px;">
+    <label for="exampleInputEmail1" class="form-label">Genre</label>
+    <input type="text" class="form-control" id="genre" name="genre" >
+  </div>
+  <div class="mb-3" style="width: 400px;">
+    <label for="exampleInputEmail1" class="form-label">Description</label>
+    <input type="text" class="form-control" id="description" name="description" >
+  </div>
 
-    <form action="{{ route('books.store') }}" method="POST">
+  <button type="submit" class="btn btn-primary">Create Book</button>
+</form>
+
+    <!-- <form action="{{ route('books.store') }}" method="POST">
         @csrf
         <div>
             <label for="name">Name:</label>
@@ -55,6 +76,8 @@
         <div>
             <button type="submit">Create Book</button>
         </div>
-    </form>
+    </form> -->
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
